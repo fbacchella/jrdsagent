bd4907be343ee71b2d472551b31f45fc2af97b66
Fabrice Bacchella <fbacchella@spamcop.net>
2009-03-24 15:35:09 +0000

    Process infos now reads from /proc/<pid> For better processing of long command lines

---
+++ src/jrds/probe/ProcessInfo.java
@@ -0,0 +1,35 @@
+package jrds.probe;
+
+import java.util.Map;
+
+public class ProcessInfo extends RMIIndexed {
+	private String indexName;
+	private long uptime = 0;
+
+	public ProcessInfo(String indexName, String pattern) {
+		super(pattern);
+		this.indexName = indexName;
+	}
+	public String getIndexName() {
+		return indexName;
+	}
+	/* (non-Javadoc)
+	 * @see jrds.probe.RMI#getNewSampleValues()
+	 */
+	@Override
+	public Map getNewSampleValues() {
+		Map retValues = super.getNewSampleValues();
+		uptime = ((Number)retValues.get("uptime")).longValue();
+		
+		retValues.remove("uptime");
+		return retValues;
+	}
+	/* (non-Javadoc)
+	 * @see jrds.probe.RMI#getUptime()
+	 */
+	@Override
+	public long getUptime() {
+		return uptime;
+	}
+	
+}
