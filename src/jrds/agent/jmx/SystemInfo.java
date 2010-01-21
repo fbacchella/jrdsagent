03c5098a938f3ae48fdc502ad743d0771598642a
Fabrice Bacchella <fbacchella@spamcop.net>
2010-01-21 12:22:33 +0000

    An universal system info probe for any JVM > 1.5, using jmx

---
+++ src/jrds/agent/jmx/SystemInfo.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package jrds.agent.jmx;
+
+import java.lang.management.ManagementFactory;
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.management.OperatingSystemMXBean;
+
+import jrds.agent.LProbe;
+
+/**
+ * @author bacchell
+ *
+ */
+public class SystemInfo implements LProbe{
+	
+	private enum OSBEAN {
+		CVM  {
+			@Override
+			public Number getValue(OperatingSystemMXBean bean) {
+				return bean.getCommittedVirtualMemorySize();
+			}
+		},
+		FPM {
+			@Override
+			public Number getValue(OperatingSystemMXBean bean) {
+				return bean.getFreePhysicalMemorySize();
+			}
+			
+		},
+		FSS {
+			@Override
+			public Number getValue(OperatingSystemMXBean bean) {
+				return  bean.getFreeSwapSpaceSize();
+			}
+		},
+		LA {
+			@Override
+			public Number getValue(OperatingSystemMXBean bean) {
+				return bean.getSystemLoadAverage();
+			}
+		},
+		TPM {
+			@Override
+			public Number getValue(OperatingSystemMXBean bean) {
+				return bean.getTotalPhysicalMemorySize();
+			}
+		},
+		TSS {
+			@Override
+			public Number getValue(OperatingSystemMXBean bean) {
+				return bean.getTotalSwapSpaceSize();
+			}
+		};
+		public abstract Number getValue(OperatingSystemMXBean bean);
+	};
+
+	public String getName() throws RemoteException {
+		return "jmxsysteminfo";
+	}
+
+	public Map<String, Number> query() throws RemoteException {
+		com.sun.management.OperatingSystemMXBean systemBean = (com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+		Map<String, Number> retValues = new HashMap<String, Number>();
+		for(OSBEAN value: OSBEAN.values()) {
+			retValues.put(value.name(), value.getValue(systemBean));
+		}
+		return retValues;
+	}
+}
