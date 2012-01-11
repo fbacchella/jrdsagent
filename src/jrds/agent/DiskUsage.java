786b7ec912817bd0655547c01ddf2b920b5f55fb
Fabrice Bacchella <fbacchella@spamcop.net>
2012-01-11 11:16:56 +0100

    Added disk usage probe

---
+++ src/jrds/agent/DiskUsage.java
@@ -0,0 +1,34 @@
+package jrds.agent;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class DiskUsage  extends LProbe{
+    String path;
+    
+    public Boolean configure(String path) {
+        this.path = path;
+        return true;
+    }
+
+    @Override
+    public Map<String, Number> query() {
+        File pathFile = new File(path);
+        if(! pathFile.isDirectory()) {
+            throw new RuntimeException("Path is not a directory");
+        }
+        Map<String, Number> values = new HashMap<String, Number>();
+        values.put("free", pathFile.getFreeSpace());
+        values.put("total", pathFile.getTotalSpace());
+        values.put("usable", pathFile.getUsableSpace());
+        return values;
+    }
+
+    @Override
+    public String getName() {
+        return "du" + path.replace('/', '_').replace(':', '_').replace('\\', '_');
+    }
+
+}
