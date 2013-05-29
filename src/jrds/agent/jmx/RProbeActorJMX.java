476b8c0a6523c246b56fa5be3121fc5f1ff1d4ae
Fabrice Bacchella <fbacchella@spamcop.net>
2013-05-29 18:34:51 +0200

    removed the dependencie to sun.management, uses now java.lang.management

--- src/jrds/agent/jmx/RProbeActorJMX.java
+++ src/jrds/agent/jmx/RProbeActorJMX.java
@@ -1,15 +1,15 @@
 package jrds.agent.jmx;
 
+import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 
-import sun.management.ManagementFactory;
 import jrds.agent.RProbeActor;
 
 public class RProbeActorJMX extends RProbeActor {
     final private long starttime;
     
     public RProbeActorJMX() {
-        RuntimeMXBean rtbean = ManagementFactory.getRuntimeMXBean();
+       RuntimeMXBean rtbean = ManagementFactory.getRuntimeMXBean();
 
         starttime = rtbean.getStartTime();
     }
