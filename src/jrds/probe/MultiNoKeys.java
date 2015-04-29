612c380438b5cf5307609ddf9c15bb904da3b0a4
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2015-04-29 21:28:27 +0200

    MuliNoKeys can now parse single line, no index status file

--- src/jrds/probe/MultiNoKeys.java
+++ src/jrds/probe/MultiNoKeys.java
@@ -7,24 +7,24 @@ import jrds.factories.ProbeBean;
 
 @ProbeBean({"index"})
 public class MultiNoKeys extends RMI implements IndexedProbe {
-    
+
     String index = null;
 
     @Override
     public Boolean configure() {
         List<Object> args = new ArrayList<Object>();
-        
+
         // The colon key index
-        Integer keyIndex = jrds.Util.parseStringNumber(getPd().getSpecific("keyIndex"), 0);
-        args.add(keyIndex);
-        
+        Integer keyIndex = jrds.Util.parseStringNumber(getPd().getSpecific("keyIndex"), -1);
+        args.add(keyIndex);            
+
         // The regex to use as a separator
         String separator = getPd().getSpecific("separator");
         if(separator == null || separator.isEmpty()) {
             separator = "\\s+";
         }
         args.add(separator);
-        
+
         if(getPd().getSpecific("remote") == null) {
             getPd().addSpecific("remote", "jrds.agent.linux.MultiNoKeys");
         }
@@ -47,5 +47,5 @@ public class MultiNoKeys extends RMI implements IndexedProbe {
     public String getIndexName() {
         return index;
     }
-    
+
 }
