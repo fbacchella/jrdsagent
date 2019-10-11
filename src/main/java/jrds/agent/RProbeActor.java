d2cf2081c4a5178c7c1d2c4fd1b5d63ce2839cbc
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2019-10-11 15:46:34 +0200

    Wrong exception to protect badly configured probes.

--- src/main/java/jrds/agent/RProbeActor.java
+++ src/main/java/jrds/agent/RProbeActor.java
@@ -89,7 +89,7 @@ public class RProbeActor {
         int index = 0;
         for(Object thisarg: args) {
             if (thisarg == null) {
-                throw new InvocationTargetException(new NullPointerException("argument " + index + " null"), "Error configuring probe " + p);
+                throw new InvocationTargetException(new IllegalArgumentException("argument " + index + " not defined"), "Error configuring probe " + p);
             }
             argsType[index] = thisarg.getClass();
             argsVal[index] = thisarg;
