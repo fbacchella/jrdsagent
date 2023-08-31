36522a7d5ff7dc20d7ab9ebeadccaaffb0be6dd2
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2023-08-31 08:52:08 +0200

    Non-strict inequality '>=' or '<=' can be replaced with '=='

--- src/main/java/jrds/agent/windows/WmiRequester.java
+++ src/main/java/jrds/agent/windows/WmiRequester.java
@@ -22,7 +22,7 @@ public abstract class WmiRequester {
         private long lastUpdate = 0;
         @Override
         public void run() {
-            if(cache.size() <= 0) {
+            if(cache.size() == 0) {
             }
             Date now = new Date();
             // Only one refresh / second
