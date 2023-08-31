f720893fbb7d127125a6caae48c582991c223dd2
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2023-08-31 12:42:10 +0200

    Improved Meminfo26 for better efficiency with Java 8. Adding new counters.

--- src/main/java/jrds/agent/linux/Meminfo26.java
+++ src/main/java/jrds/agent/linux/Meminfo26.java
@@ -3,14 +3,16 @@ package jrds.agent.linux;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import jrds.agent.Start;
-
 public class Meminfo26 extends LProbeProc {
-    private static final Pattern linePattern = Pattern.compile("(\\w+):\\s*(\\d+).*");
+
+    private static final Pattern linePattern = Pattern.compile("(\\w+)(?:\\((\\w+)\\))?:\\s*(\\d+)( kB)?.*");
+    private static final List<String> HUGE_VALUE_KEYS = List.of("HugePages_Total", "HugePages_Free", "HugePages_Rsvd", "HugePages_Surp");
 
     @Override
     public String getName() {
@@ -19,32 +21,28 @@ public class Meminfo26 extends LProbeProc {
 
     public Map<String, Number> parse(BufferedReader r) throws IOException {
         Map<String, Number> retValues = new HashMap<>();
-        //This value is not always present, put a sensible default
-        retValues.put("Hugepagesize", 2048 * 1024);
         String line;
-        while((line = r.readLine()) != null) {
-            Matcher m = linePattern.matcher(line);
-            if(m.matches()) {
+        Matcher m = linePattern.matcher("");
+        while ((line = r.readLine()) != null) {
+            m.reset(line);
+            if (m.matches()) {
                 String key = m.group(1);
-                long value = Start.parseStringNumber(m.group(2), 0L);
-                value *= 1024;
+                if (m.group(2) != null) {
+                    key += "_" +  m.group(2);
+                }
+                long value = Long.parseLong(m.group(3));
+                // If suffixed by kB, transform to bytes
+                if (m.group(4) != null) {
+                    value *= 1024;
+                }
                 retValues.put(key, value);
             }
         }
-
-        //Conversion of huge page from number to bytes
-        Number hugepagesize = retValues.remove("Hugepagesize");
-        if(hugepagesize != null) {
-            long pg_size = hugepagesize.longValue();
-            for(String hugePage_key: new String[] {"HugePages_Total", "HugePages_Free", "HugePages_Rsvd", "HugePages_Surp"}) {
-                Number value = retValues.get(hugePage_key);
-                //If the kernel is compiled without huge page
-                if(value != null) {
-                    long hugePage_value = value.longValue();
-                    hugePage_value *= pg_size;
-                    retValues.put(hugePage_key, hugePage_value);
-                }
-            }
+        // This value is not always present, use a sensible default
+        long pgSize = Optional.ofNullable(retValues.remove("Hugepagesize")).map(Number::longValue).orElse(2048L * 1024);
+        // Convert huge pages numbers to bytes
+        for (String hugePage_key: HUGE_VALUE_KEYS) {
+            retValues.computeIfPresent(hugePage_key, (k, v) -> v.longValue() * pgSize);
         }
         return retValues;
     }
