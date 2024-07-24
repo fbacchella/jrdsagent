7b0875fa8915eff8d45e77dd8f216d39eed34c49
Fabrice Bacchella <fabrice.bacchella@orange.fr>
2024-07-24 21:35:04 +0200

    Generic disk usage with FileStore

--- src/main/java/jrds/agent/DiskUsageFileStore.java
+++ src/main/java/jrds/agent/DiskUsageFileStore.java
@@ -4,14 +4,13 @@ import java.io.IOException;
 import java.nio.file.FileStore;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
 
-/**
- * This probe mimic the data from the SNMP probe PartitionSpace to be able to keep history
- */
-public class DiskUsageSnmp extends LProbe {
+public class DiskUsageFileStore extends LProbe {
 
     FileStore fs;
+    boolean asSnmp = false;
 
     public Boolean configure(String path) {
         try {
@@ -23,13 +22,28 @@ public class DiskUsageSnmp extends LProbe {
     }
 
     @Override
+    public void setProperty(String specific, String value) {
+        if (specific.equals("asSnmp")) {
+            asSnmp = Boolean.parseBoolean(value);
+        } else {
+            super.setProperty(specific, value);
+        }
+    }
+
+    @Override
     public Map<String, Number> query() {
         try {
-            return Map.of(
+            Map<String, Number> values = Map.of(
                     "Total", fs.getTotalSpace(),
-                    "Used", fs.getTotalSpace() -  fs.getUsableSpace(),
-                    "Avail", fs.getUsableSpace()
+                    "Avail", fs.getUsableSpace(),
+                    "BlockSize", fs.getBlockSize(),
+                    "Unallocated", fs.getUnallocatedSpace()
             );
+            if (asSnmp) {
+                values = new HashMap<>(values);
+                values.put("Used", fs.getTotalSpace() -  fs.getUsableSpace());
+            }
+            return values;
         } catch (IOException ex) {
             throw new CollectException("Invalid collect name: ", ex);
         }
